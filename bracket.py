33bc8ceac81b5c3e8a299f1409b8e046cebb38e4
jte2004 <jte2004@nyu.edu>
2025-03-26 23:26:38 -0400

    cleaned up bracket logic and outline for streamlit app

---
+++ bracket.py
@@ -0,0 +1,115 @@
+from dataclasses import dataclass
+import json
+import random
+
+
+@dataclass
+class Matchup:
+    team_1: int
+    team_2: int
+    winner: int | None = None
+    tie_breaker: str | None = None
+
+
+@dataclass
+class BracketRound:
+    matchups: list[Matchup]
+
+
+@dataclass
+class RegionalBracket:
+    round_w_16: BracketRound | None = None
+    round_w_8: BracketRound | None = None
+    round_w_4: BracketRound | None = None
+    round_w_2: BracketRound | None = None
+    bracket_winner: int | None = None
+
+
+@dataclass
+class FinalFour:
+    semis_1: Matchup
+    semis_2: Matchup
+    finals_matchup: Matchup | None = None
+
+
+# add a bracket data object which stores the results, rather than a list of tuples
+class BracketSimulator():
+    def __init__(self, method: str ='historical', verbose: bool = True):
+        if method not in {'historical', 'random'}:
+            raise ValueError("Need proper picking method: select from ['random', 'historical']")
+        self.method = method  # method can be ['random', 'historical']
+        self.verbose = verbose  # print out all rounds
+        with open("empirical_history_dict.json") as f:
+            self.game_probability_dict = json.load(f)
+    
+    def picker(self, seed_1: int, seed_2: int):  # -> int:
+        # need to handle even seeds in semis or finals
+        if seed_1 == seed_2:
+            return seed_1
+
+        #draw random number
+        check = random.random()
+
+        if self.method == 'historical':
+            # get better seed
+            if seed_1 <= seed_2:
+                # get prob of seed 1 winning
+                prob_seed_1 = self.game_probability_dict[str(seed_1)][str(seed_2)] / 100
+            else:
+                # get prob of seed 2 winning
+                prob_seed_1 = 1 - self.game_probability_dict[str(seed_2)][str(seed_1)] / 100
+                
+            #compare random number to probability of seed 1 winning
+            return seed_1 if check <= prob_seed_1 else seed_2
+        else:
+            return seed_1 if check < 0.5 else seed_2  
+    
+    def play_matchup(self, matchup: Matchup): # -> Matchup:
+        matchup.winner = self.picker(seed_1=matchup.team_1, seed_2=matchup.team_2)
+        if matchup.team_1 == matchup.team_2:
+            check = random.random()
+            matchup.tie_breaker = "first" if check < 0.5 else "second"
+        return matchup
+
+    def play_round(self, bracket_round: BracketRound): # -> BracketRound | int:
+        [self.play_matchup(matchup) for matchup in bracket_round.matchups]
+
+        if len(bracket_round.matchups) == 1:
+            return bracket_round.matchups[0].winner
+        
+        next_round = BracketRound(matchups=[])
+        for i in range(0, len(bracket_round.matchups), 2):
+            next_round.matchups.append(
+                Matchup(
+                    team_1=bracket_round.matchups[i].winner,
+                    team_2=bracket_round.matchups[i+1].winner
+                )
+            )
+        return next_round
+    
+    def play_regional_bracket(self, full_bracket: RegionalBracket): # -> RegionalBracket:
+        full_bracket.round_w_8 = self.play_round(full_bracket.round_w_16)
+        full_bracket.round_w_4 = self.play_round(full_bracket.round_w_8)
+        full_bracket.round_w_2 = self.play_round(full_bracket.round_w_4)
+        full_bracket.bracket_winner = self.play_round(full_bracket.round_w_2)
+        return full_bracket
+    
+    def play_final_four(
+            self,
+            regional_1: RegionalBracket,
+            regional_2: RegionalBracket,
+            regional_3: RegionalBracket,
+            regional_4: RegionalBracket
+        ):
+        final_four = FinalFour(
+            semis_1=Matchup(team_1=regional_1.bracket_winner, team_2=regional_2.bracket_winner),
+            semis_2=Matchup(team_1=regional_3.bracket_winner, team_2=regional_4.bracket_winner)
+        )
+        self.play_matchup(matchup=final_four.semis_1)
+        self.play_matchup(matchup=final_four.semis_2)
+        final_four.finals_matchup = Matchup(
+            team_1=final_four.semis_1.winner,
+            team_2=final_four.semis_2.winner,
+        )
+        self.play_matchup(matchup=final_four.finals_matchup)
+        return final_four
---
+++ coin_flip_bracket.py
@@ -0,0 +1,174 @@
+import streamlit as st
+
+from CoinFlipBracket import Bracket
+
+
+def skip_lines(n_lines: int):
+    """Function to space out the st write objects to a desired place."""
+    for i in range(n_lines):
+        st.write('')
+
+
+def print_out_first_round():
+    """Function to print out the first round matchups of the bracket."""
+    first_round_seeds = [
+        '1', '16', '8', '9',
+        '4', '13', '5', '12',
+        '3', '14', '6', '11',
+        '7', '10', '2', '15'
+    ]
+    for i, seed in enumerate(first_round_seeds):
+        st.markdown(f":gray-background[{seed}]")
+        if i % 2 == 1:
+            st.write('')
+    st.markdown(':gray[--]')
+    for i, seed in enumerate(first_round_seeds):
+        st.markdown(f":gray-background[{seed}]")
+        if i % 2 == 1:
+            st.write('')
+
+st.header('March Madness')
+
+button_cols = st.columns(3)
+st.divider()
+with button_cols[0]:
+    run_bracket_weighted = st.button("Run Weighted Coins Bracket")
+with button_cols[2]:
+    run_bracket_random = st.button("Run Random Coins Bracket")
+
+high_level_columns = st.columns(3)
+with high_level_columns[0]:
+    with st.container():
+        lower_level_columns = st.columns(5)
+        with lower_level_columns[0]:
+            print_out_first_round()
+        
+        with lower_level_columns[1]:
+            # NEED TO TRIM THIS INTO A FUNCTION
+            st.write('')
+            seed = '1'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(4)
+            seed = '8'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(3)
+            seed = '4'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(4)
+            seed = '5'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(4)
+            seed = '3'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(3)
+            seed = '6'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(4)
+            seed = '7'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(3)
+            seed = '2'
+            st.markdown(f":gray-background[{seed}]")
+
+            skip_lines(7)
+            seed = '1'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(3)
+            seed = '8'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(4)
+            seed = '4'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(4)
+            seed = '5'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(3)
+            seed = '3'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(4)
+            seed = '6'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(3)
+            seed = '7'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(4)
+            seed = '2'
+            st.markdown(f":gray-background[{seed}]")
+
+        with lower_level_columns[2]:
+            # NEED TO TRIM THIS INTO A FUNCTION
+            skip_lines(4)
+            seed = '1'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(10)
+            seed = '4'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(10)
+            seed = '3'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(9)
+            seed = '2'
+            st.markdown(f":gray-background[{seed}]")
+
+            skip_lines(14)
+            seed = '1'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(9)
+            seed = '4'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(10)
+            seed = '3'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(10)
+            seed = '2'
+            st.markdown(f":gray-background[{seed}]")
+        
+        with lower_level_columns[3]:
+            # NEED TO TRIM THIS INTO A FUNCTION
+            skip_lines(10)
+            seed = '1'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(23)
+            seed = '2'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(25)
+            seed = '1'
+            st.markdown(f":gray-background[{seed}]")
+            skip_lines(23)
+            seed = '2'
+            st.markdown(f":gray-background[{seed}]")
+
+        with lower_level_columns[4]:
+            # NEED TO TRIM THIS INTO A FUNCTION
+            skip_lines(23)
+            seed = '1'
+            st.markdown(f":green-background[{seed}]")
+            skip_lines(50)
+            seed = '2'
+            st.markdown(f":green-background[{seed}]")
+
+
+
+with high_level_columns[1]:
+    with st.container():
+        lower_level_columns = st.columns(3)
+        with lower_level_columns[1]:
+            skip_lines(38)
+            st.markdown('## :green-background[1]')
+            skip_lines(6)
+    with st.container(border=True):
+        lower_level_columns = st.columns(3)
+        with lower_level_columns[0]:
+            st.markdown('### 1')
+        with lower_level_columns[1]:
+            st.markdown('#### vs')
+        with lower_level_columns[2]:
+            st.markdown('### 1')
+    with st.container():
+        st.write('')
+
+with high_level_columns[2]:
+    with st.container():
+        lower_level_columns = st.columns(5)
+        with lower_level_columns[4]:
+            print_out_first_round()
